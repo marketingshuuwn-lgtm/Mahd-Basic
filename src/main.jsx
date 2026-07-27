c88db4f43daeef9c56e846053a80e6012f6e76c1
marketingshuuwn-lgtm <marketingshuuwn@gmail.com>
2026-07-27 03:36:24 +0300

    import Trello styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ToastProvider } from './hooks/useToast.jsx';
 import './index.css';
+import './styles/trello.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
